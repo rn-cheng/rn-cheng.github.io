2500e69d3b64575da4594aeac37b48e105b96d43
rn-cheng <82163998+rn-cheng@users.noreply.github.com>
2025-10-17 18:40:23 +0000

    Deploying to gh-pages from @ rn-cheng/RNCheng.github.io@423883f0b6b0cb790ae79bed38aa7907e032175e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "RN-Cheng/RNCheng.github.io",
+    "data-repo": "rn-cheng/RNCheng.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
